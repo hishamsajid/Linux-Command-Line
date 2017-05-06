05bf4dcc6c48ab10cc098a3352fe12d6956fc9a9
hishamsajid <muhammad.hisham@khi.iba.edu.pk>
2017-05-06 17:17:59 +0500

    threaded server client implemented

---
+++ clientold.c
@@ -0,0 +1,131 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <pthread.h>
+
+#define DATA "Half a league, half a league . . ."
+#define BUFF_SIZE 1024
+
+void *read_thread(void *ptr){
+
+
+
+}
+
+void *write_thread(void *ptr){
+
+
+
+}
+
+
+
+int main(int argc, char *argv[])
+	{
+	int sock;
+	int count;
+	int rval;
+//	pthread_t thread1;
+//	pthread_t thread2;
+
+	struct sockaddr_in server;
+	struct hostent *hp;
+	char buff_s[BUFF_SIZE];
+    char buff_r1[BUFF_SIZE];
+    char buff_r2[BUFF_SIZE];
+//	write(1,"Enter IP & Port no: ",sizeof("Enter IP & Port no: "));
+//	read(0,)
+
+	write(1," Hiham Bin Sajid -09383 \n Version 0.0.1 \n All rights reserved \n \n",
+	sizeof(" Hiham Bin Sajid -09383 \n Version 0.0.1 \n All rights reserved \n \n"));
+
+	write(1,"add -> addition \n",sizeof("add -> addition \n"));
+	write(1,"sub -> subtraction \n",sizeof("add -> subtraction \n"));
+	write(1,"mult -> multiplication \n",sizeof("add -> multiplication \n"));
+	write(1,"list -> list all processes \n \n",
+	sizeof("list -> list all processes \n \n"));
+	write(1,"	list -r -> list all running processes \n",
+		sizeof("	list -r -> list all running processes \n"));
+	write(1,"kill variations\n \n",sizeof("kill variations\n \n"));
+	write(1,"	kill -id\n",sizeof("	kill -id\n"));
+	write(1,"	kill -n\n",sizeof("	kill -n\n"));
+	write(1,"	kill -all\n",sizeof("	kill -all\n"));
+				
+
+
+	/* Create socket */
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		perror("opening stream socket");
+		exit(1);
+	}
+	/* Connect socket using name specified by command line. */
+	server.sin_family = AF_INET;
+	hp = gethostbyname(argv[1]);
+	if (hp == 0) {
+		fprintf(stderr, "%s: unknown host\n", argv[1]);
+		exit(2);
+	}
+	bcopy(hp->h_addr, &server.sin_addr, hp->h_length);
+	server.sin_port = htons(atoi(argv[2]));
+
+	if (connect(sock,(struct sockaddr *) &server,sizeof(server)) < 0) {
+		perror("connecting stream socket");
+		exit(1);
+	}
+	while(1) {
+		//sending data to server
+		write(1,"Enter Command: ",sizeof("Enter Command: "));
+		count = read(0,buff_s,1024);
+		rval = write(sock,buff_s,count);
+
+		if(rval < 0) {
+			perror("writing failed: ");
+		}
+
+		if(rval > 0) {
+			count = sprintf(buff_s,"Data sent: %d\n",rval);
+			write(1,buff_s,count);
+
+		}
+
+
+		//recieve reply from server
+
+		rval = read(sock,buff_r1,5000);
+
+	
+		buff_r1[rval-1] = '\0';
+
+		if(rval < 0){
+			perror("error recieving: ");
+		}
+
+		if(rval > 0){
+			count = sprintf(buff_r2,"Recieved: %s\n",buff_r1);
+			write(1,buff_r2,count);
+			//fflush(NULL);
+		}
+
+
+
+
+
+/*
+	if (write(sock, DATA, sizeof(DATA)) < 0)
+		perror("writing on stream socket");
+	else{
+		getchar();
+	}
+*/
+	}
+	close(sock);
+}
+
+  
+
--- server.c
+++ server.c
@@ -8,6 +8,9 @@
 #include <unistd.h>
 #include <time.h>
 #include <signal.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 #define TRUE 1
 #define BUFF_SIZE 5000
@@ -15,6 +18,7 @@
 struct process {
 
 	char *Pname;
+	pid_t client_pid;
 	int PID;
 	char *status;
 	float *s_time;
@@ -22,7 +26,23 @@ struct process {
 
 };
 
+struct struct_input {
+
+	int sock;
+	char *buff;
+	pid_t client_pid;
+
+};
+
 int p_number;
+struct process p_list[20];
+
+
+void *funct_arth(void *ptr);
+void *funct_run(void *ptr);
+void *funct_kill(void *ptr);
+void *funct_list(void *ptr);
+void *funct_read(void *ptr);
 
 
 int main()
@@ -32,17 +52,23 @@ int main()
 	int msgsock;
 	char buff[BUFF_SIZE];
 	char buff_2[BUFF_SIZE];
+	char buff_copy[BUFF_SIZE];
 	int rval;
 	char *tkptr;
 	int accept_fork;
+	
+	pthread_t t_arth;
+	pthread_t t_run;
+	pthread_t t_kill;
+	pthread_t t_list;
 
-
+	int t_rval = 0;
 	char *token;
 	char *ptr;
 	int count;
 	double sum = 0;
 	int f_val;
-	struct process p_list[20];
+
 	
 
 	/* Create socket */
@@ -76,6 +102,9 @@ int main()
 		accept_fork = fork();
 
 		if(accept_fork == 0){
+		
+		pid_t client_pid = getpid();
+
 
 		if (msgsock == -1)
 			perror("accept");
@@ -91,107 +120,65 @@ int main()
 			}
 
 			else {
-				//printf("-->%s\n", buff);
-				//fflush(NULL);
+				
 				count = sprintf(buff_2,"Recieved: %s \n",buff);
 				write(1,buff_2,count);
 
 			//enter command line server code here
 
 				buff[rval-1] = '\0';
-				token = strtok_r(buff, " ",&tkptr);
+				strcpy(buff_copy,buff);
+				token = strtok_r(buff," ",&tkptr);
 
-				if(strcmp("add",token)==0) {
-				write(1,"An addition function\n",sizeof("An addition function\n"));
+				//arithematic functions
+				if(strcmp("add",token)==0 || strcmp("mult",token)==0
+				|| strcmp("sub",token)==0) {
+				
+				struct struct_input input;
+				input.sock = msgsock;
+				input.buff = malloc(50);
+				strcpy(input.buff,buff_copy);
 				
 				
-				while(token!=NULL){
-				double x = strtod(token,&ptr);
-				sum=sum+x;
-				token = strtok_r(NULL," ",&tkptr);
-
-				}
-
-            //writing back to client
-
-                count = sprintf(buff,"%lf",sum);
-                write(1,buff,count);
-                write(msgsock,buff,count);
-				//fflush(msgsock);
-
-
-                }
-                else if(strcmp("sub",token)==0) {
-
-                sum= atof(strtok_r(NULL," ",&tkptr));
-
-                while(token!=NULL){
-
-                    sum=sum-strtod(token,&ptr);
-                    token=strtok_r(NULL," ",&tkptr);
-
-                    }
-
-                count = sprintf(buff,"%lf",sum);
-                write(1,buff,count);
-                write(msgsock,buff,count);
-
-
-               }
-
-
-               else if(strcmp("mult",token)==0) {
-            	
+				t_rval = pthread_create(&t_arth,NULL,funct_arth,(void*)&input);
 				
-			    sum = 1;
-				token = strtok_r(NULL," ",&tkptr);
-
-                while(token!=NULL){
-                    
-                    sum = sum*strtod(token,&ptr);
-					token = strtok_r(NULL," ",&tkptr);
-					}
-
-                count = sprintf(buff,"%lf",sum);
-                write(1,buff,count);
-                write(msgsock,buff,count);
-
-
                 }
 
-				
-
+				//create process
                 else if(strcmp("run",token)==0) {
+				
+			    token = strtok_r(NULL," ",&tkptr);
+				
+				struct struct_input input;
+				input.buff = malloc(50);
+				strcpy(input.buff,token);
+				input.sock = msgsock;
+				input.client_pid = client_pid;
 
-
-                token = strtok_r(NULL," ",&tkptr);
-
-                f_val = fork();
-				if(f_val != 0) {
-
-					p_list[p_number].PID = f_val;
-					p_list[p_number].Pname = (char*) malloc(50);
-					strcpy(p_list[p_number].Pname,token);
-	        		p_list[p_number].status = (char*) malloc(50);
-	                p_list[p_number].status = "active";
-	                p_list[p_number].s_time = (float*)clock();
-	                p_number++;
-				}
-
-                else if(f_val == 0){
-
-                        execlp(token,token,NULL);
-
-                    }
-
-				if(f_val != 0) {
-
-					write(msgsock,"Successfully executed", sizeof("Successfully executed"));
-
-				}
-
+				t_rval = pthread_create(&t_run,NULL,funct_run,(void*)&input);
+				
 
                 }
+
+				
+				else if(strcmp("disconnect",token)==0){
+				int itr = 0;
+				
+ 				while(itr<p_number) {
+					if(p_list[itr].client_pid == client_pid){
+						int pid = p_list[itr].PID;          
+						p_list[itr].status = "deactivated";
+        				kill(pid,SIGTERM);
+						}
+        			itr++;
+            		}
+					close(msgsock);
+					raise(SIGTERM);
+					break;
+
+					
+				}
+		
 
 				else if(strcmp("list",token)==0) {
 
@@ -199,17 +186,21 @@ int main()
 
 				while(itr<p_number) {
 
-					count = sprintf(buff,"\nPID: %d\n", p_list[itr].PID);
+					count = sprintf(buff,"\nPID: %d\nName: %s\nStatus: %s \nClient ID: %d \n", p_list[itr].PID,
+					p_list[itr].Pname,p_list[itr].status,p_list[itr].client_pid);
     				write(msgsock,buff,count);
-    				count = sprintf(buff,"\nName: %s\n", p_list[itr].Pname);
+    				/*
+					count = sprintf(buff,"\nName: %s\n", p_list[itr].Pname);
     				write(msgsock,buff,count);
     				count = sprintf(buff,"\nStatus: %s\n", p_list[itr].status);
+    				write(msgsock,buff,count);
+					count = sprintf(buff,"\nClient ID: %d\n", p_list[itr].client_pid);
     				write(msgsock,buff,count);
 
     				write(msgsock,"\n",sizeof("\n")-1);
 
 					write(msgsock,"Processes listed",sizeof("Processes listed"));
-
+					*/
     				itr++;
 
 					}
@@ -218,87 +209,25 @@ int main()
 
 				else if(strcasecmp("kill",token) == 0) {
 
-                token = strtok_r(NULL," ",&tkptr);
 
-                    if(strcmp(token,"-id")==0) {
+				struct struct_input input;
+				input.buff = malloc(50);
+				strcpy(input.buff,buff_copy);
+				input.sock = msgsock;
 
-                    token = strtok_r(NULL," ",&tkptr);
-                    int pid = atoi(token);
-                    count = sprintf(buff,"%d",pid);
-                    write(1,buff,count);
-
-
-                    int itr = 0;
-
-                    while(itr<p_number) {
-                       
-                        if(pid == p_list[itr].PID) {
-
-                            kill(pid,SIGTERM);
-                            p_list[itr].status = "deactivated";
-
-                            write(msgsock,"\nproccess killed\n", sizeof("\nproccess killed\n"));
-                           
-							break;
-                            }
-                    itr++;
-
-                        }
-
-                    }
-
-                 if(strcmp(token,"-n") == 0){
-
-                token = strtok_r(NULL," ",&tkptr);
-                int itr =0;
-
-                while(itr<p_number){
-
-                    if(strcmp(token,p_list[itr].Pname)==0){
-
-                        int pid = p_list[itr].PID;
-                        p_list[itr].status = "deactivated";
-
-                        kill(pid,SIGTERM);
-
-                        write(msgsock,"\nproccess killed\n", sizeof("\nproccess killed\n"));
-
-                        break;
-                        }
-                    itr++;
-                	}
-                }
+				t_rval = pthread_create(&t_kill,NULL,funct_kill,(void*)&input);
 
 
-                 if(strcmp(token,"-all")==0) {
 
-                int itr = 0;
-
-                while(itr<p_number) {
-
-                    int pid = p_list[itr].PID;
-                    p_list[itr].status = "deactivated";
-                    kill(pid,SIGTERM);
-                    itr++;
-                    }
-                write(msgsock,"\nAll proccesses killed\n", sizeof("\nAll proccesses killed\n"));
-                }
-
-				else {
-					
-					write(msgsock,"\n Incorrect command, try again \n",
-					sizeof("\n Incorrect command, try again \n"));
-
-				}
-
-
-            }
+            	} 
 
 			else {
 				write(msgsock,"\n Incorrect command, try again \n",
 				sizeof("\n Incorrect command, try again \n"));
 
 			}
+
+			
 
 
 //----------------------------
@@ -311,3 +240,234 @@ int main()
 	} while (TRUE);
 
 }
+
+
+
+void *funct_kill(void *ptr) {
+
+char *buff;
+int t_msgsock;
+pid_t client_pid;
+double sum = 0;
+struct struct_input *kill_;
+kill_ = (struct struct_input *)ptr;
+char *token;
+char *tkptr;
+char *strptr;
+int count;
+
+
+t_msgsock = (*kill_).sock;
+buff = (*kill_).buff;
+client_pid = (*kill_).client_pid;
+
+
+
+token = strtok_r(buff," ",&tkptr);
+token = strtok_r(NULL," ",&tkptr);
+
+if(strcmp(token,"-id")==0) {
+
+	token = strtok_r(NULL," ",&tkptr);
+	int pid = atoi(token);
+    count = sprintf(buff,"%d",pid);
+    write(1,buff,count);
+
+
+    int itr = 0;
+
+	while(itr<p_number) {
+
+    	if(pid == p_list[itr].PID) {
+
+        	kill(pid,SIGTERM);
+            p_list[itr].status = "deactivated";
+
+            write(t_msgsock,"\nproccess killed\n", sizeof("\nproccess killed\n"));
+                           
+			break;
+                	}
+            itr++;
+
+        	}
+
+        }
+
+if(strcmp(token,"-n") == 0)	{
+
+	token = strtok_r(NULL," ",&tkptr);
+    int itr =0;
+	char *deactivated = (char*)malloc(50);
+	strcpy(deactivated,"deactivated");
+	//printf(deactivated);
+    	while(itr<p_number){
+
+            if(strcmp(token,p_list[itr].Pname) == 0){
+				
+
+            	int pid = p_list[itr].PID;
+            	p_list[itr].status = "deactivated";
+
+                kill(pid,SIGTERM);
+
+                write(t_msgsock,"\nproccess killed\n", sizeof("\nproccess killed\n"));
+
+                break;
+                	}
+				
+            itr++;
+        
+			}
+        
+		 }
+
+
+if(strcmp(token,"-all")==0) {
+
+	int itr = 0;
+
+    while(itr<p_number) {
+		if(p_list[itr].client_pid == client_pid){
+		int pid = p_list[itr].PID;          
+		p_list[itr].status = "deactivated";
+        kill(pid,SIGTERM);
+		}
+        itr++;
+            }
+        write(t_msgsock,"\nAll proccesses killed\n", sizeof("\nAll proccesses killed\n"));
+    	}
+
+else {
+					
+		write(t_msgsock,"\n Incorrect command, try again \n",
+		sizeof("\n Incorrect command, try again \n"));
+
+		}
+
+
+}
+
+		
+
+
+
+
+
+
+
+void *funct_arth(void *ptr){
+
+	char *buff;
+	int t_msgsock;
+	double sum = 0;
+	struct struct_input *arth;
+	arth = (struct struct_input *)ptr;
+	char *token;
+	char *tkptr;
+	char *strptr;
+	int count;
+
+	t_msgsock = (*arth).sock;
+	buff = (*arth).buff;
+	//strcpy(buff,(*arth).buff);
+	printf("Recieved into buff %s",buff);
+
+	token = strtok_r(buff," ",&tkptr);
+
+	if(strcmp(token,"add")==0) {
+
+	write(1,"An addition function\n",sizeof("An addition function\n"));
+	while(token!=NULL){
+		double x = strtod(token,&strptr);
+		sum=sum+x;
+		token = strtok_r(NULL," ",&tkptr);				
+		}
+
+	}
+
+
+    else if(strcmp("mult",token)==0) {
+            				
+	sum = 1;
+	token = strtok_r(NULL," ",&tkptr);
+
+    while(token!=NULL){            
+    	sum = sum*strtod(token,&strptr);
+		token = strtok_r(NULL," ",&tkptr);
+		}
+
+	}
+
+
+	else if(strcmp("sub",token)==0) {
+
+    	sum= atof(strtok_r(NULL," ",&tkptr));
+
+        while(token!=NULL){
+            sum=sum-strtod(token,&tkptr);
+            token=strtok_r(NULL," ",&tkptr);
+            }
+		
+	}
+
+    //writing back to client
+    count = sprintf(buff,"%lf",sum);
+    write(1,buff,count);
+    write(t_msgsock,buff,count);
+				
+	
+}
+
+
+void *funct_run(void *ptr){
+
+	char *name;
+	int t_msgsock;
+	pid_t client_pid;
+	struct struct_input *run;
+	run = (struct struct_input *)ptr;
+
+	t_msgsock = (*run).sock;
+	name = (*run).buff;
+	client_pid = (*run).client_pid;
+
+	
+
+	printf("we have %d and %s ",t_msgsock,name);
+	
+	
+	int f_val = 0;
+	
+	f_val = fork();
+		if(f_val != 0) {
+			//mutex
+			//pthread_mutex_lock(&mutex);
+			p_list[p_number].PID = f_val;
+			p_list[p_number].Pname = (char*) malloc(50);
+			strcpy(p_list[p_number].Pname,name);
+	        p_list[p_number].status = (char*) malloc(50);
+	        p_list[p_number].status = "active";
+	        p_list[p_number].s_time = (float*)clock();
+			p_list[p_number].client_pid = client_pid;
+	       // pthread_mutex_unlock(&mutex);
+			p_number++;
+			
+				}
+
+         else if(f_val == 0){
+			//pthread_mutex_lock(&mutex);
+			write(t_msgsock,"Successfully forked\n", sizeof("Successfully forked\n"));
+        	execlp(name,name,NULL);
+			//pthread_mutex_unlock(&mutex);
+			write(t_msgsock,"unsuccesful execution\n", sizeof("unsuccesful execution\n"));
+                }
+
+		if(f_val != 0) {
+
+			//write(global_msgsock,"Successfully executed", sizeof("Successfully executed"));
+
+				}
+
+		}
+
+
